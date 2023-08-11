45b9828c4aa39f553b1c17008f35c880bb077e60
jrberganza <68700999+jrberganza@users.noreply.github.com>
2023-08-10 18:34:49 -0600

    Change page title

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -46,7 +46,7 @@ export function Page() {
           {texts.global.shopname}
         </Typography>
         <Typography variant="h6" m={1}>
-          {texts.homepage.title}
+          Deus Caritas Est 2023
         </Typography>
         <Card className="m-4" elevation={10}>
           <Grid2 container spacing={2}>
